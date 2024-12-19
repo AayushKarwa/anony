469810cda20ecba44e9d1b8fd502871a3c67b12a
Ayush karwa <ayushkarwa18@gmail.com>
2024-12-20 03:30:31 +0530

    added gpt route

--- src/app/api/get-messages/route.ts
+++ src/app/api/get-messages/route.ts
@@ -19,6 +19,24 @@ export async function GET(req:Request){
     const userId = new mongoose.Types.ObjectId(user._id)
 
     try {
+        const user = await UserModel.aggregate([
+          {$match:{id:userId}},
+          {$unwind: '$messages'},
+          {$sort:{'messages.createdAt': -1}},
+          {$group:{_id:'$_id', messages:{$push:'$messages'}}}
+        ])
+
+        if(!user || user.length === 0){
+            return Response.json({
+                success:false,
+                message:"User Not found"
+            },{status:404})
+        }
+
+        return Response.json({
+            success:true,
+            messages: user[0].messages
+        },{status:200})
         
     } catch (err) {
         
